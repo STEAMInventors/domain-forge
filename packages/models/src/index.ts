19fb2b99a698615add78651810d32fb6c6533b0c
STEAMInventors <abhattacharyya@steaminventors.com>
2026-09-25 16:30:24 -0400

    Export production model providers

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,2 +1,6 @@
+export * from './anthropic-provider.js';
+export * from './environment.js';
 export * from './fake-provider.js';
+export * from './openai-provider.js';
 export * from './policy-registry.js';
+export * from './provider-router.js';
